b3a19f0954e510de0c8617fbb1a7dc608ed6f17e
Leenux <leenux@Leenuxui-Macmini.local>
2024-01-01 15:15:53 +0900

    fix: echo no option

--- juhyelee/buildin/echo.c
+++ juhyelee/buildin/echo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhyelee <juhyelee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 15:12:54 by juhyelee          #+#    #+#             */
+/*   Updated: 2024/01/01 15:15:10 by juhyelee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,17 +19,23 @@ int	main(int argc, char *argv[])
 {
 	size_t	index;
 
-	if (argc > 2)
+	index = 1;
+	while (index < (size_t)argc)
 	{
-		perror("echo is no option");
-		exit(EXIT_FAILURE);
+		if (strlen(argv[index]) == 2 && argv[index][0] == '-')
+		{
+			perror("echo does not have option");
+			exit(EXIT_FAILURE);
+		}
+		index++;
 	}
 	index = 1;
 	while (index < (size_t)argc)
 	{
 		write(STDOUT_FILENO, argv[index], strlen(argv[index]));
+		write(STDOUT_FILENO, " ", 1);
 		index++;
 	}
 	write(STDOUT_FILENO, "\n", 1);
-	return (0);
+	exit(EXIT_SUCCESS);
 }
